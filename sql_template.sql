fd57af8c600359b5b4952e0c30d6b9cbdae4ab6e
18emilyhung <40717526+18emilyhung@users.noreply.github.com>
2020-02-07 13:14:06 +0800

    Update sql_template.sql

--- sql_template.sql
+++ sql_template.sql
@@ -1,73 +1,27 @@
-/* Welcome to the SQL mini project. For this project, you will use
-Springboard' online SQL platform, which you can log into through the
-following link:
+/*construct a single PostgreSQL statement producing the following aggregation:
+Column name         Description
+app_id              App id
+app_views           Total count of app detail views for this app
+app_installs        Total count of app installs for this app
+app_conversion_rate Total count of app installs / total app detail views for this app
+assumption that store_app_view and store_app_download events should be happening without 10 minutes
+code session: */
 
-https://sql.springboard.com/
-Username: student
-Password: learn_sql@springboard
-
-The data you need is in the "country_club" database. This database
-contains 3 tables:
-    i) the "Bookings" table,
-    ii) the "Facilities" table, and
-    iii) the "Members" table.
-
-Note that, if you need to, you can also download these tables locally.
-
-In the mini project, you'll be asked a series of questions. You can
-solve them using the platform, but for the final deliverable,
-paste the code for each solution into this script, and upload it
-to your GitHub.
-
-Before starting with the questions, feel free to take your time,
-exploring the data, and getting acquainted with the 3 tables. */
-
-
-
-/* Q1: Some of the facilities charge a fee to members, but some do not.
-Please list the names of the facilities that do. */
-
-
-/* Q2: How many facilities do not charge a fee to members? */
-
-
-/* Q3: How can you produce a list of facilities that charge a fee to members,
-where the fee is less than 20% of the facility's monthly maintenance cost?
-Return the facid, facility name, member cost, and monthly maintenance of the
-facilities in question. */
-
-
-/* Q4: How can you retrieve the details of facilities with ID 1 and 5?
-Write the query without using the OR operator. */
-
-
-/* Q5: How can you produce a list of facilities, with each labelled as
-'cheap' or 'expensive', depending on if their monthly maintenance cost is
-more than $100? Return the name and monthly maintenance of the facilities
-in question. */
-
-
-/* Q6: You'd like to get the first and last name of the last member(s)
-who signed up. Do not use the LIMIT clause for your solution. */
-
-
-/* Q7: How can you produce a list of all members who have used a tennis court?
-Include in your output the name of the court, and the name of the member
-formatted as a single column. Ensure no duplicate data, and order by
-the member name. */
-
-
-/* Q8: How can you produce a list of bookings on the day of 2012-09-14 which
-will cost the member (or guest) more than $30? Remember that guests have
-different costs to members (the listed costs are per half-hour 'slot'), and
-the guest user's ID is always 0. Include in your output the name of the
-facility, the name of the member formatted as a single column, and the cost.
-Order by descending cost, and do not use any subqueries. */
-
-
-/* Q9: This time, produce the same result as in Q8, but using a subquery. */
-
-
-/* Q10: Produce a list of facilities with a total revenue less than 1000.
-The output of facility name and total revenue, sorted by revenue. Remember
-that there's a different cost for guests and members! */
+WITH X AS (SELECT MAX(event_time_utc) AS date_of_change
+FROM store_events
+WHERE event_type = 'store_app_download')
+SELECT install.app_id , COUNT(event_type = 'store_app_view') AS app_views,
+       install.app_installs, install.app_installs/ app_view AS app_conversion_rate
+FROM (SELECT s1.app_id,
+        COUNT (CASE WHEN s2.event_time_utc <= X.date_of_change
+                AND (s1.event_type = 'store_app_view' AND s2.event_type = 'store_app_download)
+                AND (s2.event_time_utc - s1.event_time_utc <= INTERVAL '10 MINUTE') THEN 1
+                WHEN s2.event_time_utc > X.date_of_change
+                AND (s1.event_type = 'store_app_install' AND s2.event_type = 'store_app_install') THEN 1
+                ELSE NULL END) AS app_installs,
+      FROM store_events s1 JOIN store_events s2
+      ON s1.user_id = s2.user_id
+      AND s1.app_id = s2.app_id
+      GROUP BY s1.app_id) install
+FROM store_app_install
+GROUP BY install.app_id
